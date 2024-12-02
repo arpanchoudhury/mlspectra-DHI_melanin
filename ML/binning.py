b0ca0107446b255660fb2a4642b9b406b6a7aad7
Arpan Choudhury <32363671+arpanchoudhury@users.noreply.github.com>
2024-12-02 21:50:25 +0530

    Update binning.py

--- ML/binning.py
+++ ML/binning.py
@@ -2,13 +2,14 @@ import mlspectra
 
 spec_path = 'ML/Spectra'
 
-#e_min = 1.5498
-#e_max = 6.1992
+max_wavelength = 800
+min_wavelength = 200
 
-N_bin = 60
+N_bin = 24
 read_P = False            # If true, binned spectra will be loaded from 'file_spec', if false will be calculated and stored in this file
-file_spec = 'spectra_60bins_waveuniform.dat'
-file_geom = 'fingerprint_40.dat'
+resolution = int((max_wavelength-min_wavelength)/N_bin)
+file_spec = str(resolution)+'nmResolution_spectra.dat'
+file_geom = 'fingerprint.dat'
 N_fing = 40
 #Int_lam, lambda_min, dlambda = mlspectra.bin_spectra_uniform(spec_path, read_P, file_spec, file_geom, e_min, e_max, N_bin, N_fing, 'qc_Opt.dat', 'dihedral_b3lyp_Opt.dat')
 
